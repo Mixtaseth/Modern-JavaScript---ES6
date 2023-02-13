e766e90ec153cb63001013800dfaf517272f171b
Mixtaseth <mixtaseth@gmail.com>
2023-02-13 19:30:32 +0100

    modified my js file

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // GLOBAL SELECTORS
+const MAX_CHARS = 150;
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
+const feedbackListEl = document.querySelector('.feedbacks');
+const submitBtnEl = document.querySelector('.submit-btn');
 
 // --COUNTER COMPONENT--
 
 const inputHandler = () => {
   //determine the max number of character
-  const maxNrChars = 150;
+  const maxNrChars = MAX_CHARS;
   //determine the number of characters currently typed
   const nrCharsTyped = textareaEl.value.length;
   //calculate the number of characters left (maximum - typed)
@@ -18,7 +22,7 @@ const inputHandler = () => {
 
 textareaEl.addEventListener('input', inputHandler);
 
-// SUBMIT COMPONENT
+// FORM COMPONENT
 
 const submitHandler = (event) => {
   // To prevent default browser action
@@ -50,10 +54,38 @@ const submitHandler = (event) => {
 
   //we have the text so we need to extract other info from the text
   const hashtag = text.split(' ').find((word) => word.includes('#'));
-  // const company;
-  // const badgeLetter;
-  // const upvoteCount = 0;
-  // const daysAgo = 0;
+  const company = hashtag.substring(1);
+  const badgeLetter = company.substring(0, 1).toUpperCase();
+  const upvoteCount = 0;
+  const daysAgo = 0;
+
+  // new feedback Item HTML
+  const feedbackItemHTML = `
+    <li class="feedback">
+      <button class="upvote">
+          <i class="fa-solid fa-caret-up upvote__icon"></i>
+          <span class="upvote__count">${upvoteCount}</span>
+      </button>
+      <section class="feedback__badge">
+          <p class="feedback__letter">${badgeLetter}</p>
+      </section>
+      <div class="feedback__content">
+          <p class="feedback__company">${company}</p>
+          <p class="feedback__text">${text}</p>
+      </div>
+      <p class="feedback__date">${daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
+    </li>
+  `;
+
+  // insert new feedbacks in list
+  feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+
+  // clear text area
+  textareaEl.value = ' ';
+  // blur the submit button
+  submitBtnEl.blur();
+  // reset counter
+  counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener('submit', submitHandler);
